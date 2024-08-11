678a4cabbc4379212b711da848443621188c9338
Sachin Rawat <108470710+sachinsingh2003@users.noreply.github.com>
2024-08-11 21:48:46 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        double[] marks = new double[numSubjects];
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextDouble();
+        }
+
+        double totalMarks = calculateTotalMarks(marks);
+        double averagePercentage = calculateAveragePercentage(totalMarks, numSubjects);
+        String grade = calculateGrade(averagePercentage);
+
+        displayResults(totalMarks, averagePercentage, grade);
+    }
+
+    private static double calculateTotalMarks(double[] marks) {
+        double totalMarks = 0;
+        for (double mark : marks) {
+            totalMarks += mark;
+        }
+        return totalMarks;
+    }
+
+    private static double calculateAveragePercentage(double totalMarks, int numSubjects) {
+        return (totalMarks / numSubjects) / 100 * 100;
+    }
+
+    private static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A";
+        } else if (averagePercentage >= 80) {
+            return "B";
+        } else if (averagePercentage >= 70) {
+            return "C";
+        } else if (averagePercentage >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    private static void displayResults(double totalMarks, double averagePercentage, String grade) {
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+    }
+}
